f5e6d102cfb6199214f7600c1f25d68a44dc4129
qiuyugong-aifi <82243255+qiuyugong-aifi@users.noreply.github.com>
2021-08-30 13:42:15 -0700

    add HISE Function

---
+++ R/fileDescToDataframe.R
@@ -0,0 +1,33 @@
+#' Title
+#'
+#' @param descriptors
+#' @param keep_labs
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fileDescToDataframe <- function(descriptors,
+                                keep_labs = FALSE) {
+
+  assertthat::assert_that(typeof(descriptors) == "list")
+  assertthat::assert_that(typeof(keep_labs) == "logical")
+
+  do.call(
+    rbind,
+    lapply(
+      descriptors,
+      function(desc) {
+        desc <- unlist(desc)
+        desc <- desc[!grepl("scheme", names(desc))]
+        names(desc) <- sub("^descriptors.","",names(desc))
+        if(!keep_labs) {
+          desc <- desc[!grepl("^lab", names(desc))]
+        }
+        desc <- as.list(desc)
+        df <- as.data.frame(desc)
+        df
+      }
+    )
+  )
+}
