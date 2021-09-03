0cb560713a65bfd188e18a6725d3c140e8475bd0
qiuyugong-aifi <82243255+qiuyugong-aifi@users.noreply.github.com>
2021-09-02 22:08:12 -0700

    add plotly

--- R/BatchDonors_DataAvailablity_Dotsplots.R
+++ R/BatchDonors_DataAvailablity_Dotsplots.R
@@ -8,15 +8,12 @@
 #'
 #' @examples
 BatchDonors_DataAvailablity_Dotsplots <- function(rna_dataframe, meta_data = "sample.visitName") {
-  plot_info <- rna_dataframe %>%
-    dplyr::select((!!as.name(meta_data)), subject.subjectGuid, file.batchID)
-
+  plot_info <- rna_dataframe %>% dplyr::select((!!as.name(meta_data)),
+                                               subject.subjectGuid, file.batchID)
   dot_shapes <- factor(plot_info[, meta_data])
-
-  p <- ggplot(plot_info, aes(x = subject.subjectGuid, y = file.batchID, group = (!!as.name(meta_data)))) +
-    geom_point(alpha = 8, aes(shape = dot_shapes, size = 120)) +
-    scale_shape_manual(values = c(3, 4, 5, 6, 0, 1, 2)) +
-    theme(text = element_text(size = 18), axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
-
+  p <-ggplot(plot_info, aes(x = subject.subjectGuid, y = file.batchID, group = (!!as.name(meta_data)))) +
+    geom_point(alpha = 1, size = 2.5, aes(shape = dot_shapes,colour=factor((!!as.name(meta_data))))) +
+    scale_shape_manual(values = c(3, 4, 5, 6, 0, 1, 2,22,23,24,25)) +
+    theme(text = element_text(size = 12), axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
   return(p)
 }
--- R/GeneDistribution_Violin_Plot.R
+++ R/GeneDistribution_Violin_Plot.R
@@ -1,20 +1,24 @@
 #' Title
 #'
 #' @param combined_dataset
+#' @param plotly
 #'
 #' @return
 #' @export
 #'
 #' @examples
-GeneDistribution_Violin_Plot<- function(combined_dataset) {
-
-
-
-  p<-ggplot(combined_dataset[[]], aes(x=batch_id, y=n_genes, color=batch_id)) +
-    geom_violin(trim=FALSE)+geom_boxplot(width=0.1)
-
-
-  return (p)
-
-
+GeneDistribution_Violin_Plot<-function (combined_dataset,plotly = "False") {
+  if (plotly == "False") {
+    p <- ggplot(combined_dataset[[]], aes(x = batch_id, y = n_genes,
+                                          color = batch_id)) + geom_violin(trim = FALSE) + geom_boxplot(width = 0.1)
+  }
+  else if (plotly == "True") {
+    p <- combined_dataset[[]]%>%
+      plot_ly(
+        x = ~batch_id,
+        y =  ~n_genes,
+        split = ~batch_id,
+        type = 'violin',box = list(visible = T),meanline = list(visible = T))
+  }
+  return(p)
 }
