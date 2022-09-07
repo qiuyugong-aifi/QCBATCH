642fe238343213880a0aa0b383afb6e051c1b695
qiuyugong-aifi <82243255+qiuyugong-aifi@users.noreply.github.com>
2022-09-07 10:36:00 -0700

    Update fileDescToDataframe.R

--- R/fileDescToDataframe.R
+++ R/fileDescToDataframe.R
@@ -7,8 +7,9 @@
 #' @export
 #'
 #' @examples
-fileDescToDataframe <- function(descriptors,
+fileDescToDataframe_beta <- function(descriptors,
                                 keep_labs = FALSE) {
+  descriptors<-descriptors[[1]]
   assertthat::assert_that(typeof(descriptors) == "list")
   assertthat::assert_that(typeof(keep_labs) == "logical")
 
@@ -19,10 +20,42 @@ fileDescToDataframe <- function(descriptors,
       function(desc) {
         desc <- unlist(desc)
         desc <- desc[!grepl("scheme", names(desc))]
-        names(desc) <- sub("^descriptors.", "", names(desc))
-        if (!keep_labs) {
-          desc <- desc[!grepl("^lab", names(desc))]
-        }
+        names(desc) <- sub("^descriptors.","",names(desc))
+        desc <- desc[!grepl("^specimens", names(desc))]
+        desc <- desc[!grepl("^lab", names(desc))]
+        desc <- desc[!grepl("^emr", names(desc))]
+        desc <- desc[!grepl("^survey", names(desc))]
+        desc <- desc[!grepl("^revision", names(desc))]
+        desc <- desc[!grepl("^file.userTags", names(desc))]
+        desc <- as.list(desc)
+        df <- as.data.frame(desc)
+        df
+      }
+    )
+  )
+}
+
+fileDescToDataframe <- function(descriptors,
+                                keep_labs = FALSE) {
+
+  assertthat::assert_that(typeof(descriptors) == "list")
+  assertthat::assert_that(typeof(keep_labs) == "logical")
+
+  do.call(
+    rbind,
+    lapply(
+      descriptors,
+      function(desc) {
+        desc <- unlist(desc)
+        desc <- desc[!grepl("scheme", names(desc))]
+        names(desc) <- sub("^descriptors.","",names(desc))
+        desc <- desc[!grepl("^specimens", names(desc))]
+        desc <- desc[!grepl("^lab", names(desc))]
+        desc <- desc[!grepl("^emr", names(desc))]
+        desc <- desc[!grepl("^survey", names(desc))]
+        desc <- desc[!grepl("^revision", names(desc))]
+        desc <- desc[!grepl("^file.userTags", names(desc))]
+
         desc <- as.list(desc)
         df <- as.data.frame(desc)
         df
